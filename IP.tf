3c77aee38c1d790ebe5ff986a6bbbedf90ce233e
Rania Fahmy <rania.fahmy222@gmail.com>
2024-04-20 23:07:53 +0200

    updated .gitignore and IP.tf

--- IP.tf
+++ IP.tf
@@ -4,8 +4,8 @@ resource "google_compute_global_address" "lb_ipv4" {
 }
 
 # Create IPv6 address for forwarding rule
-resource "google_compute_global_address" "lb_ipv6" {
-  name         = "http-lb-ipv6"
-  ip_version   = "IPV6"
-  address_type = "EXTERNAL"
-}
+# resource "google_compute_global_address" "lb_ipv6" {
+#   name         = "http-lb-ipv6"
+#   ip_version   = "IPV6"
+#   address_type = "EXTERNAL"
+# }
